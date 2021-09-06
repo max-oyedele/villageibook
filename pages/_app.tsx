744856b8703723a56358d2a20e2667daf56c91be
maximdymchenko <likewagon@outlook.com>
2021-09-06 04:56:14 +0200

    inital main

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider as ThemeProvider } from "@chakra-ui/react";
+import theme from "styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
---
+++ styles/theme.ts
@@ -0,0 +1,50 @@
+import { extendTheme, Theme } from '@chakra-ui/react';
+import { mode, GlobalStyleProps } from '@chakra-ui/theme-tools';
+import { SystemStyleObject } from '@chakra-ui/system';
+
+const theme: Theme = extendTheme({
+  config: {
+    useSystemColorMode: false,
+    initialColorMode: 'light',
+  },
+  colors: { primary: 'teal.600' },
+  fonts: {
+    heading: `'Archivo Narrow', sans-serif`,
+    body: `'Barlow', sans-serif`,
+    mono: `Menlo, Monaco, Consolas, Liberation Mono, Courier New, monospace`,
+  },
+  fontWeights: {
+    bold: 700,
+    heading: 500,
+  },
+  styles: {
+    global: (props: GlobalStyleProps): SystemStyleObject => ({
+      body: {
+        fontFamily: 'body',
+        color: mode('gray.700', 'whiteAlpha.900')(props),
+        bg: mode('gray.50', 'gray.800')(props),
+        transition: 'background-color 0.2s',
+        lineHeight: 'calc(8px + 2ex)',
+      },
+
+      '*::placeholder': {
+        color: mode('gray.400', 'whiteAlpha.400')(props),
+      },
+      '*, *::before, &::after': {
+        borderColor: mode('gray.200', 'whiteAlpha.300')(props),
+        wordWrap: 'break-word',
+      },
+      '#__next': {
+        minHeight: '100vh',
+      },
+      hr: {
+        borderColor: mode('gray.700', 'whiteAlpha.900')(props),
+      },
+      pre: {
+        overflowX: 'auto',
+      },
+    }),
+  },
+});
+
+export default theme;
