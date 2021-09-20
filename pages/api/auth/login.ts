841aa93e44aa8a5c5c0d8518686728d4a4c5fa36
maximdymchenko <likewagon@outlook.com>
2021-09-20 13:17:49 +0200

    put log auth

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -16,20 +16,20 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
   headers["authorization"] = "Basic " + Buffer.from("villageibook-client" + ":" + "4C6JYPsCJ795vFVS").toString("base64");
   headers["content-type"] = "application/x-www-form-urlencoded";
-console.log('headers', headers)
+// console.log('headers', headers)
   try {
     const { data, headers: returnedHeaders } = await axios.post(
       "https://villageibook-api.abosit.com/oauth/token", // api backend path
       params,
-      {
-        headers
-      }
       // {
-      //   headers: {
-      //     "authorization": "Basic " + Buffer.from("villageibook-client" + ":" + "4C6JYPsCJ795vFVS").toString("base64"),
-      //     "content-type": "application/x-www-form-urlencoded"
-      //   }
+      //   headers
       // }
+      {
+        headers: {
+          "authorization": "Basic " + Buffer.from("villageibook-client" + ":" + "4C6JYPsCJ795vFVS").toString("base64"),
+          "content-type": "application/x-www-form-urlencoded"
+        }
+      }
     );
     
     //  Update headers on requester using headers from Node.js server response
@@ -40,10 +40,7 @@ console.log('headers', headers)
     res.send(data); // Send data from Node.js server response
   } catch (error) {
     // Send status (probably 401) so the axios interceptor can run.
-    // console.log('server err', error)
-    // res.status(status).json(data);
-    // res.status(401).json(error.response?.data)
-    // res.status(401).json(error.response?.data)
-    res.status(401).json(headers)
+    console.log('server err', error.response?.data)
+    res.status(401).json(error.response?.data)
   }
 };
