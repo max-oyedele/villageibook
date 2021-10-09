79bf051a31edf63a0c1ec6d0484fc5323c962de2
maximdymchenko <likewagon@outlook.com>
2021-10-09 14:23:08 +0200

    add me endpoint

---
+++ pages/api/users/me.ts
@@ -0,0 +1,51 @@
+import { fetchUserToken } from "helpers/fetch-user-token";
+import { fetchWrapper } from "helpers/fetch-wrapper";
+
+import { usersRepo } from "helpers/user-repo";
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+async function handler(req, res) {
+  const {username, password} = req.query;
+
+  const { access_token } = await fetchUserToken({username: username, password: password});
+
+  switch (req.method) {
+    case "GET":
+      return getMe();
+    
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
+  }
+
+  async function getMe() {
+    try {
+      let me = fetchWrapper.get(
+        baseUrl + `/users/${req.query.uuid}.json`,
+        access_token
+      );
+
+      await me.then((response) => {
+        res.status(200).json(response);
+      });
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
+  }
+
+  function updateUser() {
+    try {
+    //   usersRepo.update(req.query.id, req.body);
+      return res.status(200).json({});
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
+  }
+
+  function deleteUser() {
+    // usersRepo.delete(req.query.id);
+    return res.status(200).json({});
+  }
+}
+
+export default handler;
