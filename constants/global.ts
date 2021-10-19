45c8329d7165d47c94c196239f619ee3668bd91c
maximdymchenko <likewagon@outlook.com>
2021-10-19 03:37:55 +0200

    add post endpoint

--- constants/global.ts
+++ constants/global.ts
@@ -6,3 +6,42 @@ export const platformCountries = [
     uuid: "000",
   },
 ];
+
+export const watchCountries = [
+  {
+    id: 0,
+    name: "Bangladesh",
+    href: "bangladesh",
+    uuid: "000"
+  },
+  {
+    id: 1,
+    name: "United Kingdom",
+    href: "uk",
+    uuid: "001"
+  },
+  {
+    id: 2,
+    name: "United States",
+    href: "usa",
+    uuid: "002"
+  },
+  {
+    id: 3,
+    name: "Australia",
+    href: "australia",
+    uuid: "003"
+  },
+  {
+    id: 4,
+    name: "Canada",
+    href: "canada",
+    uuid: "004"
+  },
+  {
+    id: 5,
+    name: "Other",
+    href: "other",
+    uuid: "005"
+  },
+]
